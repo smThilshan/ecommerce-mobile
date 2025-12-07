3763c5a6facbe6aad401daf1b0c42c2692e35724
smThilshan <smthilshan@gmail.com>
2025-12-07 20:45:52 +0300

    initial commit

---
+++ lib/presentation/splash/page/splash.dart
@@ -0,0 +1,23 @@
+import 'package:ecommerce_mobile/core/configs/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: const Center(
+        child: Text(
+          'Splash Screen',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
